1b4168a4d25c16c32853d9d3662df345727f4fd9
Cristian-Lazzari <cristian.lazzari.cl@gmail.com>
2024-08-27 19:02:59 +0200

    as

--- src/pages/CheckOut.vue
+++ src/pages/CheckOut.vue
@@ -474,8 +474,12 @@
             <span>{{ d.month }}</span>
           </div>
         </div>
-        <img src="../../public/img/arrow.png" @click="modMonth('add')" v-if="year.length > 1" class="next">
-        <img src="../../public/img/arrow.png" @click="modMonth('rem')" v-if="year.length > 1" class="previus">
+        <svg @click="modMonth('add')" v-if="year.length > 1" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-arrow-left-square next" viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm11.5 5.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z"/>
+        </svg>
+        <svg @click="modMonth('rem')" v-if="year.length > 1" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-arrow-left-square previus" viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm11.5 5.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z"/>
+        </svg>
       </div>
     </section>
     <section v-if="day">
@@ -767,8 +771,7 @@ section{
       display: flex;
       justify-content: center;
       align-items: center;
-      border-radius: 6px;
-      padding: 7px;
+      border-radius: 2px;
     }
     .next{
       transform: translate(50%,-50%) rotateZ(180deg);
